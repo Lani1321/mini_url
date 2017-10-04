24b3be1c8db07b87da0e9cd7492ce9710b9d80c0
Lani1321 <lani.berry13@gmail.com>
2017-10-04 14:43:42 -0400

    validate original url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
+  validates :long_url, :format => URI::regexp(%w(http https))
+
   def generate_short_url
     self.short_url = SecureRandom.urlsafe_base64(6)
     self.save!
