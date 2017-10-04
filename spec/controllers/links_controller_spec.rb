b5f6675be4a4ab164cbf82122653e726c518943c
Lani1321 <lani.berry13@gmail.com>
2017-10-04 11:16:43 -0400

    tests for links controller

---
+++ spec/controllers/links_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require_relative '../app/controllers/links_controller'
+
+RSpec.describe "mini_url-LinksTest", :type => :request do
+  describe "LinksController" do
+    describe 'GET index' do
+      it 'should get index' do
+        
+      end
+    end
+
+    describe 'GET new' do
+      it 'should get new' do
+      
+      end
+    end
+
+    describe 'POST create' do
+      it 'should create new link if it does not exist' do
+      
+      end
+
+      it 'should increment every time a link is clicked' do
+      end
+    end
+
+    describe 'POST link' do
+      it 'should show link' do
+      
+      end
+    end
+
+  end
+
+end
