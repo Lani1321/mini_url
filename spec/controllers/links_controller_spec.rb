4099e60a98690b485e90301b9ae0ed69734e5d21
Lani1321 <lani.berry13@gmail.com>
2017-10-05 10:54:31 -0400

    add tests for links controller

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
-# require_relative '../app/controllers/links_controller'
+require_relative '../app/controllers/links_controller'
+# require_relative '../app/models/link'
 
-RSpec.describe "mini_url-LinksTest", :type => :request do
+RSpec.describe "mini_url-LinksTest", :type => :controller do
   describe "LinksController" do
     describe 'GET index' do
       it 'should get index' do
@@ -17,10 +18,22 @@ RSpec.describe "mini_url-LinksTest", :type => :request do
 
     describe 'POST create' do
       it 'should create new link if it does not exist' do
-      
+        expect{
+          post :create, params: { link: attributes_for(:link) }
+        }.to change(Link, :count).by(1)
       end
 
-      it 'should increment every time a link is clicked' do
+      context "with an existing url" do
+        before :each do
+          @link = create(:link, full_url: 'pinterest.com')
+          @link.sanitize
+          @link.save
+        end
+        it "does not create a new entry" do
+          expect{
+            post :create, params: { link: attributes_for(:link, full_url: 'www.pinterest.com') }
+          }.to_not change(Link, :count)
+        end
       end
     end
 
