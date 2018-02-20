5a32784606eb43a6c39a647c9f0a0164076bd1cc
Lani1321 <lani.berry13@gmail.com>
2018-02-20 15:24:26 -0500

    modify tests for link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,21 +1,23 @@
 require "rails_helper"
 
-RSpec.describe Link, :type => :model do
-
+RSpec.describe Link, '#sanitize', :type => :model do
   it "sanitizes the url" do 
     netflix = Link.create!(full_url: "https://netflix.com")
 
     expect(netflix.sanitize).to eq("http://netflix.com")
   end
+end
 
+RSpec.describe Link, '#find_duplicate', :type => :model do
   it "finds duplicate urls" do
     netflix = Link.create!(full_url: "https://netflix.com")
 
     expect(netflix.find_duplicate.nil?).to eq(false)
-  end
+  end 
+end
 
+RSpec.describe Link, '#valid_url', :type => :model do
   it "should require a valid url" do
-
     expect(Link.new(:full_url => "hello")).to be_invalid
   end
 end
